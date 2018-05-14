224f8c3116bc03fbf5d80b32b37eca07b50a1a2d
Calle Lacouture <celacouture@gmail.com>
2018-05-14 16:37:14 -0500

    new user modal

---
+++ models/parents.js
@@ -0,0 +1,38 @@
+module.exports=function(sequeliz, DataTypes){
+  var Parent=sequelize.define("Parent",{
+    first_name:{
+      type:DataTypes.STRING,
+      allowNull:false,
+      validate: {
+          len: [2],
+          notEmpty: true
+      }
+    },
+    last_name:{
+      type:DataTypes.STRING,
+      allowNull:false,
+      validate:{
+        len:[2],
+        notEmpty:true
+      }
+    },
+    email:{
+      type:DataTypes.STRING,
+      allowNull:false,
+      validate:{
+        len:[2],
+        notEmpty:true
+      }
+    },
+    password:{
+      type:DataTypes.STRING,
+      allowNull:false,
+      validate:{
+        len:[2],
+        notEmpty:true
+      }
+    }
+
+    }
+  })
+}
